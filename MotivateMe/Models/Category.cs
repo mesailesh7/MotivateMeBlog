fa3ead7178cba393d774a28b745939a12639c9cc
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-12-12 14:20:14 -0700

    Category Model Created

---
+++ MotivateMe/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotivateMe.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Category Name is Required")]
+        [MaxLength(100, ErrorMessage = "Category Name cannot exceed 100 Characters")]
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
