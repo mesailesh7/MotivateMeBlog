f694c8e868acb2535a26c480e3432e29fcf1646e
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-12-12 14:15:52 -0700

    Post Model Created

---
+++ MotivateMe/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotivateMe.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Title Required")]
+        [MaxLength(400, ErrorMessage = "Title cannot exceed 400 Character")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Content Required")]
+        public string Content { get; set; }
+        [Required(ErrorMessage = "Author Required")]
+        [MaxLength(100, ErrorMessage = "Author name cannot exceed 100 Character")]
+        public string  Author { get; set; }
+        public string FeatureImagePath { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime PublishedDate { get; set; } = DateTime.Now;
+    }
+}
