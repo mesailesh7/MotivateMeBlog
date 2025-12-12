d39d6349e97da9159ebd0ef9c9bb0c59754e4ac3
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-12-12 15:06:01 -0700

    Database Connection added

---
+++ MotivateMe/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotivateMe.Models;
+
+namespace MotivateMe.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
--- MotivateMe/Program.cs
+++ MotivateMe/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MotivateMe.Data;
+
 namespace MotivateMe
 {
     public class Program
@@ -8,6 +11,11 @@ namespace MotivateMe
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection");
+            });
 
             var app = builder.Build();
 
