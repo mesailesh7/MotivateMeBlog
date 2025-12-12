ac5b81cf37aabeb3373cd33fedcff3fad2ce7c04
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-12-12 15:13:29 -0700

    Bug Fixed and Db created

--- MotivateMe/Program.cs
+++ MotivateMe/Program.cs
@@ -14,7 +14,7 @@ namespace MotivateMe
 
             builder.Services.AddDbContext<AppDbContext>(options =>
             {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
 
             var app = builder.Build();
