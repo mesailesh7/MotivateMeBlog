139c3560a835f7e8075ffc6bd0977b452f17c1ab
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-12-12 14:20:22 -0700

    Comment Model Created

---
+++ MotivateMe/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotivateMe.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "User Name is Required")]
+        [MaxLength(100, ErrorMessage = "User Name cannot exceed 100 Characters")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CommentDate { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Comment Content is Required")]
+        public string Content { get; set; }
+
+    }
+}
