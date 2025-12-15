1dc8fc401fb5aaa2d11894343005ae0885918c86
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-12-15 15:04:04 -0700

    Post Controller created

---
+++ MotivateMe/Controllers/PostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MotivateMe.Controllers;
+
+public class PostController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
