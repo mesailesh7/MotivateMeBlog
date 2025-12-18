510d4715d18f0763140942d56aa69edecd96f8a3
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2025-12-18 02:21:55 -0700

    create and picture upload done

--- MotivateMe/Controllers/PostController.cs
+++ MotivateMe/Controllers/PostController.cs
@@ -9,11 +9,22 @@ namespace MotivateMe.Controllers
     public class PostController : Controller
     {
         private readonly AppDbContext _context;
-        public PostController(AppDbContext context)
+        private readonly string[] _allowedExtensions = {".jpg", ".jpeg", ".png"};
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        
+        public PostController(AppDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
-        // GET
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+            // GET
+        [HttpGet]
         public IActionResult Create()
         {
             var postViewModel = new PostViewModel();
@@ -26,6 +37,70 @@ namespace MotivateMe.Controllers
             ).ToList();
 
             return View(postViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(PostViewModel postViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var inputFileExtension = Path.GetExtension(postViewModel.FeatureImage.FileName).ToLower();
+                
+                bool isAllowed =  _allowedExtensions.Contains(inputFileExtension);
+
+                if (!isAllowed)
+                {
+                    ModelState.AddModelError("", "Invalid Image format. Allowed Format are .jpg .jpeg .png");
+                    
+                    return View(postViewModel);
+                }
+
+                postViewModel.Post.FeatureImagePath = await UploadFiletoFolder(postViewModel.FeatureImage);
+                
+                await _context.Posts.AddAsync(postViewModel.Post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+
+
+
+
+            }
+            return View(postViewModel);
+        }
+
+        private async Task<string> UploadFiletoFolder(IFormFile file)
+        {
+            var inputFileExtension = Path.GetExtension(file.FileName);
+
+            var fileName = Guid.NewGuid().ToString() + inputFileExtension;
+            
+            var wwwrootPath = _webHostEnvironment.WebRootPath;
+            var imagesFolderPath = Path.Combine(wwwrootPath, "images");
+
+            if (!Directory.Exists(imagesFolderPath))
+            {
+                Directory.CreateDirectory(imagesFolderPath);
+            }
+            
+            var filePath = Path.Combine(imagesFolderPath, fileName);
+            try
+            {
+                await using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error upload images: " + ex.Message;
+            }
+            
+            return "/images/" + fileName;
+            
+            
+            
+
         }
     }
 }
